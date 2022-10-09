d7ed7210fbcc7443ec2c6c0586dc02475fe762d1
Aliaksei Rachkouski <arachkouski@colemanrg.com>
2022-10-09 16:14:14 +0300

    init app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Aside from './components/aside/Aside';
+import './App.scss';
+
+function App() {
+  return (
+	<Aside></Aside>
+  );
+}
+
+export default App;
---
+++ src/components/aside/Aside.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from 'src/styles/components/aside.module.scss';
+
+export default function Aside() {
+	return (
+		<aside className={styles.aside}>
+			Aside
+		</aside>
+	);
+}
